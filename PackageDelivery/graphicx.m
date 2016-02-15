c9c4ce3561840496a43e88dea8de4169dc971602
dlavell <dlavell@ucsc.edu>
2016-02-15 13:26:07 -0800

    removed generated files

--- PackageDelivery/graphicx.m
+++ PackageDelivery/graphicx.m
@@ -16,9 +16,9 @@ vidObj.FrameRate = 1/step;
 open(vidObj);
 frame = 1;
 figure('Units','centimeters','Position',[0 0 34 17])
-h = create_axis([1, 0.1 ,0.1 , 0.1],[1 0.1 0.1 0.1]);
+h = [1, 0.1 ,0.1 , 0.1];%create_axis([1, 0.1 ,0.1 , 0.1],[1 0.1 0.1 0.1]);
 %T = 100;
-axes(h)
+axes
 for t2 = 0:step:T
     I = find(t>=t2,1);
     if I >=length(R)
--- PackageDelivery/runPackageSim.m
+++ PackageDelivery/runPackageSim.m
@@ -4,7 +4,7 @@ warning ('off','all');
 display('Initializing Sim...')
 load('initialize_PackageDeliverySim.mat');
 T = 100;                        % Change time scale
-ref.time = 0:.2:20;             % correct for non-linear time variable
+ref.time = 0:.2:50;             % correct for non-linear time variable
 
 display('Running Sim...')
 sim PackageDeliverySim          % Run Simulation
