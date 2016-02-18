c79bf7ac92baff77786bf2259604601dfbd8402b
dlavell <dlavell@ucsc.edu>
2016-02-18 10:51:17 -0800

    edited sim and graphicx to simulate and plot second copter

--- PackageDelivery/graphicx.m
+++ PackageDelivery/graphicx.m
@@ -5,6 +5,8 @@ load('SimResults.mat')
 %Rcam = angle2dcm(pi,0,0,'YZX');
 Rcam = [-1 0 -0; 0 1 0; 0 0 -1];
 p_i = Rcam*state.signals.values';
+p_i_1 = Rcam*state1.signals.values';
+
 pdt = Rcam*ref.signals.values(:,1:3)';
 t = ref.time;
 
@@ -45,18 +47,31 @@ for t2 = 0:step:T
     hold on
     cla
     leg(1) = plot3(pdt(1,1:I)',pdt(2,1:I)',pdt(3,1:I)');
-    leg(2) = plot3(p_i(1,1:I)',p_i(2,1:I)',p_i(3,1:I)','r-');
+    leg(2) = plot3(p_i(1,1:I)',p_i(2,1:I)',p_i(3,1:I)','r-'); % first quad
+    leg(3) = plot3(p_i_1(1,1:I)',p_i_1(2,1:I)',p_i_1(3,1:I)','b-'); % second quad
     quiver3(P0(1,:),P0(2,:),P0(3,:),P1(1,:),P1(2,:),P1(3,:),'LineWidth',2,'Color','k');
     grid on
+    
     p_star = p_i(:,I);
+    p_star1 = p_i_1(:,I);
+    
     R_star = Rcam*R(:,:,I)';
     scale = 0.5;
+    
     P0B = repmat(p_star,1,3);
+    P0B1 = repmat(p_star1,1,3);
+    
     P1B = R_star*scale;
     
     quiver3(P0B(1,:),P0B(2,:),P0B(3,:),P1B(1,:),P1B(2,:),P1B(3,:),'LineWidth',1,'Color','k');
+    quiver3(P0B1(1,:),P0B1(2,:),P0B1(3,:),P1B(1,:),P1B(2,:),P1B(3,:),'LineWidth',1,'Color','k');
+    
     quad_plot(p_star,R_star,0,[],0.5,scale);
+    quad_plot(p_star1,R_star,0,[],0.5,scale);
+    
     P0B = repmat(p_star,1,4);
+    P0B1 = repmat(p_star1,1,4);
+    
     set(gca,'Xlim',[-5 30], 'YLim', [-5 30], 'ZLim', [0 40]);
     view(37.5,30);axis square;
     legend(leg,{'Reference Trajectory','Actual Trajectory'},'location','NorthEast')
