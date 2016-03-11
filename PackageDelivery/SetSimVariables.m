31132c2d052084af291393255d37d8b1dbe9e19a
dlavell <dlavell@ucsc.edu>
2016-03-10 16:18:14 -0800

    adding set sim variables function

---
+++ PackageDelivery/SetSimVariables.m
@@ -0,0 +1,74 @@
+
+T = 4000;
+ref.time = 0:.0001:T*3;
+
+
+%% Initial condition:
+    % sets initial condition for integrator
+    % sets warehouse origin in destination generator
+IC = Simulink.Parameter;
+IC.DataType = 'double';
+
+%% max Radius:
+    % sets maximum radius for deliveries around current warehouse
+maxRadius = Simulink.Parameter;
+maxRadius.DataType = 'double';
+
+%% request generator:
+%
+pop_density = 1;
+sim_start = 0;
+sim_step = .001;
+sim_stop = 40000;
+MAX_REQUEST_PER_SIM = 500;
+%
+req_que = Simulink.Parameter;
+req_que.DataType = 'double';
+req_que.Value = req_queue_generator(pop_density,sim_start,sim_step,sim_stop,MAX_REQUEST_PER_SIM);
+
+
+%% current waypoint:
+curr_wp = Simulink.Signal;
+curr_wp.DataType = 'double';
+curr_wp.Complexity = 'real';
+curr_wp.SamplingMode = 'Sample based';
+
+%% current trajectory point:
+curr_tp = Simulink.Signal;
+curr_tp.DataType = 'double';
+curr_tp.Complexity = 'real';
+curr_tp.SamplingMode = 'Sample based';
+
+%% list of destinations:
+destinations = Simulink.Signal;
+destinations.DataType = 'double';
+destinations.Complexity = 'real';
+destinations.SamplingMode = 'Sample based';
+
+%% Queue of requests:
+current_que = Simulink.Signal;
+current_que.DataType = 'double';
+current_que.Complexity = 'real';
+current_que.SamplingMode = 'Sample based';
+
+%% Queue of requests:
+n_quads = Simulink.Signal;
+n_quads.DataType = 'double';
+n_quads.Complexity = 'real';
+n_quads.SamplingMode = 'Sample based';
+
+%% Position of quadcopters:
+pos = Simulink.Signal;
+pos.DataType = 'double';
+pos.Complexity = 'real';
+pos.SamplingMode = 'Sample based';
+
+%% Time stamp:
+time_stamp = Simulink.Signal;
+time_stamp.DataType = 'double';
+time_stamp.Complexity = 'real';
+time_stamp.SamplingMode = 'Sample based';
+
+
+
+
