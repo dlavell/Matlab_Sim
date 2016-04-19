3d49fe005091327237e5118758a56183b5ad5b02
dlavell <dlavell@ucsc.edu>
2016-04-19 12:13:09 -0700

    obstacle avoidance seems to be working with one destination 2 trajectories... going to extend to two destinations now

--- PackageDelivery/shortest_path.m
+++ PackageDelivery/shortest_path.m
@@ -24,21 +24,21 @@ end
 
 
 obstacles = [
-    0     0     0     0     0     0     0     0     0     0     0     0     0     0     0;
-    0     0     0     0     0     0     0     0     0     0     0     0     0     0     0;
-    0     0     0     0     0     0     0     0     0     0     0     0     0     0     0;
-    0     0     0     0     0     0     0     1     0     0     0     0     0     0     0;
-    0     0     0     0     0     0     0     0     0     0     0     0     0     0     0;
-    0     0     0     0     0     0     0     0     0     0     0     0     0     0     0;
-    0     0     0     0     0     0     0     0     1     0     0     1     0     0     0;
-    0     0     0     1     1     1     1     0     0     0     1     0     0     0     0;
-    0     0     1     0     0     0     0     0     1     1     0     0     0     0     0;
-    0     0     0     0     0     0     0     0     0     1     1     1     0     0     0;
-    0     0     0     0     0     0     0     0     0     0     0     0     0     0     0;
-    0     0     0     0     0     0     0     0     0     0     0     0     0     0     0;
-    0     0     0     0     0     0     0     0     0     0     0     0     0     0     0;
-    0     0     0     0     0     0     0     0     0     0     0     0     0     0     0;
-    0     0     0     0     0     0     0     0     0     0     0     0     0     0     0];
+    0     0     0     0     0     0     0     0     0     0     0     0     0     0     0; % 1 - 15
+    0     0     0     0     0     0     0     0     0     0     0     0     0     0     0; % 16 - 30
+    0     0     0     0     0     0     0     0     0     0     0     0     0     0     0; % 31 - 45
+    0     0     0     0     0     0     0     1     0     0     0     0     0     0     0; % 46 - 60
+    0     0     0     0     0     0     0     0     0     0     0     0     0     0     0; % 61 - 75
+    0     0     0     0     0     0     0     0     0     0     0     0     0     0     0; % 76 - 90
+    0     0     0     0     0     0     0     0     1     0     0     1     0     0     0; % 91 - 105
+    0     0     0     1     1     1     1     0     0     0     1     0     0     0     0; % 106 - 120
+    0     0     1     0     0     0     0     0     1     1     0     0     0     0     0; % 121 - 135
+    0     0     0     0     0     0     0     0     0     1     1     1     0     0     0; % 136 - 150
+    0     0     0     0     0     0     0     0     0     0     0     0     0     0     0; % 151 - 165
+    0     0     0     0     0     0     0     0     0     0     0     0     0     0     0; % 166 - 180
+    0     0     0     0     0     0     0     0     0     0     0     0     0     0     0; % 181 - 195
+    0     0     0     0     0     0     0     0     0     0     0     0     0     0     0; % 196 - 210
+    0     0     0     0     0     0     0     0     0     0     0     0     0     0     0];% 211 - 225
 
 graph = zeros(length(obstacles(1,:))*length(obstacles(:,1)));
 rowLength = length(obstacles(1,:));
@@ -50,14 +50,12 @@ lng_resolution = (lng_max - lng_min)/colLength;
 graph = LabelNeighbors(graph,obstacles, debug);
 
 % Source node xy coordinates on grid
-S = 1;
 s_x = floor((lat_s - lat_min)/lat_resolution) + 1;
 s_y = floor((lng_s - lng_min)/lng_resolution) + 1;
 % Source node index on graph
 s_node = mod(s_x,rowLength) + rowLength*(s_y - 1);
 
 % Destination node xy coordinates on grid
-D = 45;
 d_x = floor((lat_d - lat_min)/lat_resolution) + 1;
 d_y = floor((lng_d - lng_min)/lng_resolution) + 1;
 % Destination node index on graph
@@ -68,6 +66,10 @@ d_node = mod(d_x,rowLength) + rowLength*(d_y - 1);
 % Dijkstra's shortest path algorithm
 [p, c, f] = dijkstra(graph, s_node, d_node);
 path_len = length(p);
+if(path_len ~= c) 
+    path_len;
+    c;
+end
 
 path_ = zeros(path_len,2);
 path = zeros(15,2);
@@ -75,25 +77,28 @@ path = zeros(15,2);
 % get path in terms of grid...
 for i = 1:path_len
    Gridcol = mod(p(i),rowLength);
-   Gridrow = ceil(p(i)/colLength);
+   Gridrow = ceil(p(i)/rowLength); % was /colLength
    if(Gridcol == 0) Gridcol = rowLength; end
-   lat = lat_min + Gridrow*lat_resolution;
-   lng = lng_min + Gridcol*lng_resolution;
+   lat = lat_min + Gridcol*lat_resolution;
+   lng = lng_min + Gridrow*lng_resolution;
    path_(i,:) = [lat lng];
    %if(debug)fprintf('[%d %d] ',Gridrow,Gridcol);end
 end
 %if(debug)fprintf('\nPath length = %d\n',path_len);end
 
 path(1:path_len,:) = path_;
-path;
+s_node;
+d_node;
+p;
+path_;
 path_len;
 
 end
 
 function [ path , path_len ] = exit()
-path = 0;
-path_len = 0;
-return
+    path = 0;
+    path_len = 0;
+    return
 end
 
 function graph = LabelNeighbors(G, obstacles, debug)
@@ -108,7 +113,7 @@ colLength = length(obstacles(:,1));
 
 for NodeID = 1:numberOfNodes
     Gridcol = mod(NodeID, rowLength);
-    Gridrow = ceil(NodeID/colLength);
+    Gridrow = ceil(NodeID/rowLength); % was /colLength
     if(Gridcol == 0) Gridcol = rowLength; end
 %     if(debug)
 %         fprintf('NodeID : %d\n',NodeID);
